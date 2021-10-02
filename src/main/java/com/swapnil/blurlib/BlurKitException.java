08585af338e4ab9bacd4cd3a63199d1cd0d01848
Swapnil <swapnilrajharsh@gmail.com>
2021-10-02 22:28:58 +0530

    Done

---
+++ src/main/java/com/swapnil/blurlib/BlurKitException.java
@@ -0,0 +1,9 @@
+package com.swapnil.blurlib;
+
+public class BlurKitException extends Exception {
+
+    public BlurKitException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/swapnil/blurlib/MainActivity.java
@@ -0,0 +1,13 @@
+package com.swapnil.blurlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
